3029e52d602c5d005e0a6e6016c3b93e0c487521
dinhquyle <dinhquyle@gmail.com>
2019-07-06 14:30:45 +0700

    update

---
+++ dist/libs/header.php
@@ -0,0 +1,103 @@
+<!-- Google Tag Manager -->
+<!-- End Google Tag Manager -->
+
+<header class="header">
+  <div class="inHeader">
+    <div class="box1 clearfix">
+      <ul class="left-box pc">
+        <li>
+          <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-fb.png" alt="facebook"></a>
+        </li>
+        <li>
+          <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-tt.png" alt="twiter"></a>
+        </li>
+        <li>
+          <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-yb.png" alt="youtube"></a>
+        </li>
+        <li>
+          <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-ins.png" alt="instagram"></a>
+        </li>
+      </ul>
+      <h1 class="mid-box">
+        <a href="<?php echo APP_URL ?>"><img src="<?php echo APP_ASSETS ?>img/common/header/logo.png" alt="lavo"></a>
+      </h1>
+      <div class="right-box pc">
+        <div class="lang">
+          <a href="javascript: void(0);">
+            <img src="<?php echo APP_ASSETS ?>img/common/header/flag_en.png" alt="en">
+          </a>
+          <a href="javascript: void(0);">
+            <img src="<?php echo APP_ASSETS ?>img/common/header/flag_vn.png" alt="vi">
+          </a>
+        </div>
+        <div class="search-box">
+          <span class="txt">search</span>
+          <p class="input">
+            <input type="text" value="" name="search" class="txt-search" />
+            <button class="btn-seach" name="btn-seach">Search</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div> 
+  <div class="gnav_box"> 
+    <p class="hamberger"><span class="ham"> </span><span class="ber"> </span><span class="ger"> </span></p>
+    <div class="layerMenu">
+      <div class="inLayer">
+        <ul class="social-box sp">
+          <li>
+            <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-fb.png" alt="facebook"></a>
+          </li>
+          <li>
+            <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-tt.png" alt="twiter"></a>
+          </li>
+          <li>
+            <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-yb.png" alt="youtube"></a>
+          </li>
+          <li>
+            <a href="javascript: void(0);"><img src="<?php echo APP_ASSETS ?>img/common/header/icon-ins.png" alt="instagram"></a>
+          </li>
+        </ul>
+        <div class="control-box sp">
+          <div class="lang">
+            <a href="javascript: void(0);">
+              <img src="<?php echo APP_ASSETS ?>img/common/header/flag_en.png" alt="en">
+            </a>
+            <a href="javascript: void(0);">
+              <img src="<?php echo APP_ASSETS ?>img/common/header/flag_vn.png" alt="vi">
+            </a>
+            <span class="txt">search</span>
+          </div>
+          <div class="search-box">
+            <input type="text" value="" name="search" class="txt-search" />
+            <button class="btn-seach" name="btn-seach">Search</button>
+          </div>
+        </div>
+        <ul class="gNavi">
+          <li>
+            <a href="javascript: void(0);">Trang Chủ</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Giới Thiệu</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Thương Hiệu</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Kỹ Thuật</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Tin Tức</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Tuyển Dụng</a>
+          </li>
+          <li>
+            <a href="javascript: void(0);">Liên Hệ</a>
+          </li>
+        </ul>
+        <p class="close_layer"><span>Close</span></p>
+      </div>
+    </div>
+  </div>
+</header>
